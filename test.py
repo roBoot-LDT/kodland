d12e549fb39d6e3fcee105a0da4c893a86bcf2d0
roBoot <maksim.nemkov.ldt@gmail.com>
2023-02-19 10:59:34 +0300

    test

--- test.py
+++ test.py
@@ -1,36 +1,18 @@
+import time
+from selenium import webdriver
 from bs4 import BeautifulSoup
+from webdriver_manager.chrome import ChromeDriverManager
 
-html_doc = """
-<html>
+driver = webdriver.Chrome(ChromeDriverManager().install())
 
-<head>
-    <title>
-        example
-    </title>
-</head>
+driver.get("http://www.python.org")
+soup = BeautifulSoup(driver.page_source, features="lxml")
+print(soup.prettify())
 
-<body>
-    <ul>
-        <li>Главная страница</li>
-        <li>Товары</li>
-        <li>Контакты</li>
-        <li>Корзина</li>
-    </ul>
-    <div>
-        <p>Информация о чем то тут..</p>
-        <p>И тут..</p>
-    </div>
-    <div>
-        <input type="text" placeholder="Напиши" class="input-form">
-    </div>
-</body>
-
-</html>
-"""
-
-soup = BeautifulSoup (html_doc, 'html.parser')
-
-
-
-for link in soup.find_all('li'):
-    print(link.text)
+for link in soup.find_all('a'):
+    url = link.get('href')
+    if 'https' in url or 'http' in url:
+        driver.get(url)
+    else:
+        driver.get("https://www.python.org"+url)
+    time.sleep(3)
