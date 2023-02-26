3a12887b8c60b82b004acd26c1b01a30155a209b
roBoot <maksim.nemkov.ldt@gmail.com>
2023-02-26 10:56:47 +0300

    test

--- test.py
+++ test.py
@@ -6,8 +6,9 @@ from webdriver_manager.chrome import ChromeDriverManager
 driver = webdriver.Chrome(ChromeDriverManager().install())
 
 
-for page in range(101):
+for page in range(1, 101):
   driver.get(f"https://nekdo.ru/page/{page}")
   time.sleep(1)
+  
 
       
