0ef0d5b45872fafc344a4975597b6413efe54d4a
roBoot <maksim.nemkov.ldt@gmail.com>
2023-02-26 11:35:40 +0300

    test

--- test.py
+++ test.py
@@ -5,11 +5,10 @@ from webdriver_manager.chrome import ChromeDriverManager
 
 driver = webdriver.Chrome(ChromeDriverManager().install())
 
-for i in range(1, 100):
-  url = f'https://nekdo.ru/page/{i}/'
-  driver.get(url)
-  soup = BeautifulSoup(driver.page_source, features="lxml")
-  divs = soup.find('div', class_='text')
-  for div in divs:
-      print(div.text)
-  time.sleep(2)
+url = 'https://author.today/reader/230092'
+driver.get(url)
+soup = BeautifulSoup(driver.page_source, features="lxml")
+divs = soup.find('div', id='text-container')
+for p in divs:
+    print(p.text)
+time.sleep(2)
