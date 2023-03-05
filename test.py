2e58d1b3d3b6177db5059c67c582a0c720a2e81c
roBoot <maksim.nemkov.ldt@gmail.com>
2023-03-05 10:18:29 +0300

    test

--- test.py
+++ test.py
@@ -1,14 +1,19 @@
 import time
+import pickle
 from selenium import webdriver
 from bs4 import BeautifulSoup
 from webdriver_manager.chrome import ChromeDriverManager
 
 driver = webdriver.Chrome(ChromeDriverManager().install())
-
-url = 'https://author.today/reader/230092'
-driver.get(url)
 soup = BeautifulSoup(driver.page_source, features="lxml")
-divs = soup.find('div', id='text-container')
-for p in divs:
-    print(p.text)
-time.sleep(2)
+
+url = 'https://www.python.org/downloads/'
+driver.get(url)
+
+pythons = []
+
+divs = soup.find_all('span', class_='release-number')
+for div in divs:
+    pythons.append(div.text)    
+
+print(pythons)
