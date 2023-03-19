ad731ffab9f3bcee01c0fe06c6a418de9609314c
roBoot <maksim.nemkov.ldt@gmail.com>
2023-03-19 11:23:22 +0300

    test

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 
 driver = webdriver.Chrome(ChromeDriverManager().install())
-driver.get("http://127.0.0.1:5000/")
+driver.get("http://127.0.0.1:5000/contacts")
 time.sleep(1)
 
 login = driver.find_element(By.NAME, 'login')
