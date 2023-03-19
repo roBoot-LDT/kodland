7b7c2ba025d91f0f4afe1e23b1859b0c96fcefa6
roBoot <maksim.nemkov.ldt@gmail.com>
2023-03-19 10:58:20 +0300

    еуые

--- test.py
+++ test.py
@@ -1,20 +1,12 @@
-def number_squared(x):
-    return x**2
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
 
-############################## TEST ##########################
+driver = webdriver.Chrome(ChromeDriverManager().install())
+driver.get("http://127.0.0.1:5000/")
 
-def test_number_squared():
-    assert number_squared(1) == 1
-    assert number_squared(2) == 4
-    assert number_squared(3) == 9
-    assert number_squared(4) == 16
-    assert number_squared(5) == 25
-    assert number_squared(6) == 36
-    assert number_squared(7) == 49
-    assert number_squared(8) == 64
-    assert number_squared(9) == 81
-    assert number_squared(10) == 100
-    assert number_squared(11) == 121
-    assert number_squared(12) == 144
+login = driver.find_element(By.NAME, "login")
+password = driver.find_element(By.NAME, "password")
+button = driver.find_element(By.TAG_NAME, "button")
 
-        
+print(login, password, button)
