96a91cdd844ec7156f7fc10dbeb81026cc036999
roBoot <maksim.nemkov.ldt@gmail.com>
2023-03-19 11:03:53 +0300

    test

--- test.py
+++ test.py
@@ -1,12 +1,12 @@
+import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 
 driver = webdriver.Chrome(ChromeDriverManager().install())
 driver.get("http://127.0.0.1:5000/")
+time.sleep(1)
 
-login = driver.find_element(By.NAME, "login")
-password = driver.find_element(By.NAME, "password")
-button = driver.find_element(By.TAG_NAME, "button")
-
-print(login, password, button)
+login = driver.find_element(By.NAME, 'login')
+login.send_keys('admin')
+time.sleep(4)
