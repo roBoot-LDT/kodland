234e7350fb8ddb2c871ac147e9053dfe88f87628
roBoot <maksim.nemkov.ldt@gmail.com>
2023-03-26 10:41:26 +0300

    test

--- test.py
+++ test.py
@@ -10,7 +10,12 @@ login = driver.find_element(By.NAME, 'login')
 password = driver.find_element(By.NAME, 'password')
 button = driver.find_element(By.TAG_NAME, 'button')
 
+time.sleep(1)
 login.send_keys("mnemkov7")
+time.sleep(1)
 password.send_keys("*******")
+time.sleep(1)
 button.click()
 time.sleep(2)
+driver.get("https://platform.kodland.org/en/task_56512/")
+###нажать на кнопку "сдать"
