ce63f972934241c96896358c33efeb161f327132
roBoot <maksim.nemkov.ldt@gmail.com>
2023-03-26 10:33:02 +0300

    tess

--- test.py
+++ test.py
@@ -4,18 +4,13 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 
 driver = webdriver.Chrome(ChromeDriverManager().install())
-driver.get("http://127.0.0.1:5000/")
+driver.get("https://platform.kodland.org/auth/")
 
 login = driver.find_element(By.NAME, 'login')
-password = driver.find_element(By.NAME,'password')
+password = driver.find_element(By.NAME, 'password')
 button = driver.find_element(By.TAG_NAME, 'button')
 
-login.send_keys('admin')
-time.sleep(1)
-password.send_keys('admin')
-time.sleep(1)
+login.send_keys("mnemkov7")
+password.send_keys("*******")
 button.click()
-time.sleep(1)
-contacts = driver.find_element(By.NAME, 'contacts')
-contacts.click()
-time.sleep(3)
+time.sleep(2)
