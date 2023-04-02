9d54203b4252b8486b5dc3eba647a8e5ddcf6990
roBoot <maksim.nemkov.ldt@gmail.com>
2023-04-02 10:28:51 +0300

    test

--- test.py
+++ test.py
@@ -1,29 +1,9 @@
 import time
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.common.by import By
 
 driver = webdriver.Chrome(ChromeDriverManager().install())
-driver.get("https://platform.kodland.org/auth/")
 
-login = driver.find_element(By.NAME, 'login')
-password = driver.find_element(By.NAME, 'password')
-button = driver.find_element(By.TAG_NAME, 'button')
-
-time.sleep(1)
-login.send_keys("mnemkov7")
-time.sleep(1)
-password.send_keys("******")
-time.sleep(1)
-button.click()
-time.sleep(2)
-driver.get('https://platform.kodland.org/en/task_56514/')
-time.sleep(1)
-button1 = driver.find_element(By.ID, 'sub-button')
-time.sleep(1)
-button1.click()
-time.sleep(1)
-button2 = driver.find_element(By.ID, "submit_task_button")
-time.sleep(1)
-button2.click()
-time.sleep(3)
+id = input("Введите id проекта: ")
+driver.get(f'https://hub.kodland.org/ru/projects/{id}')
+time.sleep(10)
