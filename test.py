9e71b37a2ae7e59d8630ac08b2afa97da129b11d
roBoot <maksim.nemkov.ldt@gmail.com>
2023-04-09 19:20:02 +0300

    test

--- test.py
+++ test.py
@@ -1,4 +1,19 @@
 import time
-now = time.time()
-if now - time.time() >= 60:
-    print("Good bye!")
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+driver = webdriver.Chrome(ChromeDriverManager().install())
+driver.get("https://platform.kodland.org/auth/")
+
+time.sleep(1)
+login = driver.find_element(By.NAME, 'login')
+password = driver.find_element(By.NAME, 'password')
+ 
+button = driver.find_element(By.TAG_NAME, 'button')
+
+login.send_keys('fmuhin5')
+time.sleep(1)
+password.send_keys('EwfyIgtC0v')
+time.sleep(1)
+button.click()
+time.sleep(2)
