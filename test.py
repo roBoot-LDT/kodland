5321f14a0680f09e8c7d0b14d8ed6b71cd1b06ab
roBoot <maksim.nemkov.ldt@gmail.com>
2023-04-18 21:33:34 +0300

    test

--- test.py
+++ test.py
@@ -12,3 +12,12 @@ time.sleep(10)
 soup = BeautifulSoup(driver.page_source, features="lxml")
 project_name = soup.find('h3', class_='post-title pt-3 pr-3').text
 info_tags = soup.find_all('span', class_='stat__number')
+
+data = []
+for info_tag in info_tags:
+    data.append(info_tag.text)
+
+likes, comments, remasters, stars, views = data
+print(f'Проект {project_name}')
+print('=========================')
+print(f'Лайки {likes} | Комментарии {comments} | Ремастер {remasters} | Звезды {stars} | Просмотры {views} |')
