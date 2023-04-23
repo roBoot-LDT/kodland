9dc4a9b6fb62ac7681c2ee360688c2a9bf422933
roBoot <maksim.nemkov.ldt@gmail.com>
2023-04-23 10:31:43 +0300

    test

--- m4y1/main.py
+++ m4y1/main.py
@@ -1,153 +1,46 @@
-from flask import Flask, render_template, request, url_for, redirect, jsonify
-from datetime import datetime, timedelta
-from random import randint
-from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user
-import re
+import os
+import requests
+import discord
+from discord.ext import commands
+from random import randint, choice
+intents = discord.Intents.all()
+client = discord.Client(intents=intents)
+API_URL = 'https://7015.deeppavlov.ai/model'
+bot = commands.Bot(command_prefix='$', intents=intents)
 
-app = Flask(__name__)
-
-all_orders = []
-
-app.config.update(
-    SECRET_KEY='WOW SUCH SECRET'
-)
-
-login_manager = LoginManager()
-login_manager.init_app(app)
-login_manager.login_view = "login"
+def request_sentiment(message):
+    data = {'x': [message]}
+    res = requests.post(API_URL, json=data).json()
+    santiment = res[0][0]
+    return santiment
 
 
-@login_manager.user_loader
-def load_user(login):
-    if login == 'admin':
-        return User(login)
-
-
-class User(UserMixin):
-    def __init__(self, id):
-        self.id = id
-
-
-@app.route('/')
-@login_required
-def index():
-    return render_template('index.html')
-
-
-@app.route('/products')
-@login_required
-def products():
-    product = [{'name': 'Супер крутой товар',
-                'description': 'Это супер крутой товар, скорее покупай',
-                'price': 50,
-                'img': '/static/images/2.jpg'},
-               {'name': "test",
-                'description': 'sth',
-                'price': 100,
-                'img': "/static/images/1.jpg'"}]
-    return render_template('products.html', product=product)
-
-
-@app.route('/cart')
-@login_required
-def cart():
-    return render_template('cart.html')
-
-
-@app.route('/contacts')
-@login_required
-def contacts():
-    return render_template('contacts.html')
-
-
-@app.route('/about')
-@login_required
-def about():
-    return render_template('about.html')
-
-
-@app.route('/product1')
-@login_required
-def product1():
-    date = datetime.strptime('2022.12.25', '%Y.%m.%d')
-    delta = date - datetime.now()
-    days_before_end = delta.days
-    end_date = datetime.now() + timedelta(days=14)
-    end_date = end_date.strftime('%d.%m.%Y')
-    return render_template('product1.html', action_name='Весенние скидки', end_date=end_date, days_before_end=days_before_end, number=10)
-
-
-@app.route('/product2')
-@login_required
-def product2():
-    return render_template('product2.html')
-
-
-@app.route('/lootbox')
-@login_required
-def lootbox():
-    num = randint(1, 100)
-    if num < 50:
-        chance = 50
-    elif 50 < num < 95:
-        chance = 45
-    elif 95 < num < 99:
-        chance = 4
-    else:
-        chance = 1
-    return render_template('lootbox.html', chance=chance)
-
-
-@app.route('/order', methods=['GET', 'POST'])
-@login_required
-def order():
-    if request.method == 'POST':
-        for key in request.form:
-            if request.form[key] == '':
-                return render_template('order.html', error='Не все поля заполнены!')
-            if key == 'phone_number':
-                if not re.match('\\+7\\d{9}', request.form[key]):
-                    return render_template('order.html', error='Неправильный формат номера телефона')
-        all_orders.append(request.form)
-
-    return render_template('order.html')
-
-
-@app.route('/order_list')
-@login_required
-def order_list():
-    return render_template('order_list.html')
-
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    login = 'admin'
-    password = 'admin'
-    if request.method == 'POST':
-        if request.form['login'] == login and request.form['password'] == password:
-            user = User(login)  # Создаем пользователя
-            login_user(user)  # Логинем пользователя
-            return redirect(url_for('index'))
-        else:
-            return render_template('login.html', error='Неправильный логин или пароль')
-    return render_template('login.html')
-
-
-@app.route("/registration")
-def registration():
-    return render_template("registration.html")
-
-
-@app.route("/logout")
-def logout():
-    logout_user()
-    return "Пока!"
-
-
-@app.route('/api/orders')
-def api_orders():
-    return jsonify(all_orders)
-
-
-if __name__ == "__main__":
-    app.run()
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+@bot.command('stats')
+async def send(ctx):
+    d = {}
+    with open('sentiment.txt', 'r', encoding='utf-8') as f:
+        data = f.read()
+        lines = data.split('\n')[:-1]
+        for line in lines:
+            author = line.split(' ')[0]
+            sentiment = line.split(' ')[-1]
+            if author in d:
+                d[author][sentiment] += 1
+            else:
+                d[author] = {}
+                d[author]['positive'] = 0
+                d[author]['neutral'] = 0
+                d[author]['negative'] = 0
+        for author in d:
+            for sentiment in d[author]:
+                await ctx.send(author, sentiment, d[author][sentiment])
+                # print(author, sentiment, d[author][sentiment])
+    sentiment = request_sentiment(ctx.content)
+    with open('sentiment.txt', 'a', encoding='utf-8') as f:
+        f.write(f'{ctx.author} {ctx.content} {sentiment}\n')
+client.run("MTAyMDk0NTcwMDgxNDU5NDA0OA.Ge9NUF.7zET8xJ6QzLc2-rY-ROoVodfwMelYxgjwc9MTA")
+bot.run("MTAyMDk0NTcwMDgxNDU5NDA0OA.Ge9NUF.7zET8xJ6QzLc2-rY-ROoVodfwMelYxgjwc9MTA")
