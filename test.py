91425f1e69d25507bcac4bbd3c215104351771dd
roBoot <maksim.nemkov.ldt@gmail.com>
2023-04-29 17:26:49 +0300

    test

--- test.py
+++ test.py
@@ -1,1 +1,7 @@
-print("heLlo world")
+from random import choice
+letters = "1234567890!@#$%^&*()qwertyuiop[]asdfghjkl;'zxcvbnm,./QWERTYUIOP{}ASDFGHJKL:ZXCVBNM<>?"
+ques = input("Какой длины пароль? ")
+answer = []
+for letter in range(int(ques)):
+    answer.append(choice(letters))
+print(*answer)
