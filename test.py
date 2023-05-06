d6f0567c4b79031ee92692dfc93b8b775b2c191b
roBoot <maksim.nemkov.ldt@gmail.com>
2023-05-06 17:11:51 +0300

    test

--- test.py
+++ test.py
@@ -1,10 +1,25 @@
-import random
+import discord
 
-def gen_pass(pass_length):
-    elements = "+-/*!&$#?=@<>"
-    password = ""
+# Переменная intents - хранит привилегии бота
+intents = discord.Intents.default()
+# Включаем привелегию на чтение сообщений
+intents.message_content = True
+# Создаем бота в переменной client и передаем все привелегии
+client = discord.Client(intents=intents)
 
-    for i in range(pass_length):
-        password += random.choice(elements)
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
 
-    return password
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if message.content.startswith('$hello'):
+        await message.channel.send("Hi!")
+    elif message.content.startswith('$bye'):
+        await message.channel.send("\\U0001f642")
+    else:
+        await message.channel.send(message.content)
+
+client.run("ТУТ ДОЛЖЕН БЫТЬ TOKEN ТВОЕГО БОТА")
