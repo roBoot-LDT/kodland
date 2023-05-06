531c3e053b3e6ff57f4fb040fef3de57713f4b6c
roBoot <maksim.nemkov.ldt@gmail.com>
2023-05-06 17:10:50 +0300

    test

--- test.py
+++ test.py
@@ -1,33 +1,10 @@
-import time
-from selenium import webdriver
-from bs4 import BeautifulSoup
-from webdriver_manager.chrome import ChromeDriverManager
-from discord.ext import commands
-import discord
-profile1 = 'Light_coloured'
-player1 = 'Cucumber'
-intents = discord.Intents.all()
-bot = commands.Bot(command_prefix='/', intents=intents)
+import random
 
-@bot.command('networth')
-async def on_ctx(ctx):
-    global player, profile
-    driver = webdriver.Chrome(ChromeDriverManager().install())
-    url = f"https://spillager.live/skyblock/networth/{player1}/{profile1}" #Light_coloured #Cucumber 
-    driver.get(url)
-    soup = BeautifulSoup(driver.page_source, features="lxml")
-    main = soup.find(class_='overview_element_value mt-2')
-    await ctx.send(main.text)
-@bot.command('player')
-async def on_ctx(ctx):
-    global player1
-    player1 = ctx.author
-    await ctx.send('yes')
-@bot.command('profile')
-async def on_ctx(ctx):
-    global profile1
-    profile1 = ctx.author
-    await ctx.send('yes')
-    
+def gen_pass(pass_length):
+    elements = "+-/*!&$#?=@<>"
+    password = ""
 
-bot.run('')
+    for i in range(pass_length):
+        password += random.choice(elements)
+
+    return password
