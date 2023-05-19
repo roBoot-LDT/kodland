068926a9f6301397897a2182c6b1257c57bcef5a
roBoot <maksim.nemkov.ldt@gmail.com>
2023-05-19 09:15:21 +0300

    IM GENIUS

--- test.py
+++ test.py
@@ -1,19 +1,12 @@
 
 def twoSum(nums, target):
-    if len(nums) == 2:
-         return [0, 1]
-    for first in nums:
-        if first >= target:
-            continue
+    seen = {}
+    for id, number in enumerate(nums):
+        if target - number in seen:
+            return [id, seen[target-number]]
         else:
-            answer = [nums.index(first)]
-            for second in nums:
-                if second >= target or nums.index(first) == nums.index(second):
-                        continue
-                else:
-                     if second + first == target:
-                          answer.append(nums.index(second))
-                          return answer
-print(twoSum([2, 7, 11, 15, 16, 17], 9))
+            seen[number] = id
+print(twoSum([2,7,11,15], 9))
 print(twoSum([3,2,4], 6))
 print(twoSum([3,3], 6))
+print(twoSum([3,2,3], 6))
