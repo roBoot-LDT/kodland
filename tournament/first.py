1b0e8aa053a649c8a3c5ed1054d2502582fc6bcc
roBoot <maksim.nemkov.ldt@gmail.com>
2023-05-20 21:48:19 +0300

    ooooops

--- tournament/first.py
+++ tournament/first.py
@@ -14,9 +14,11 @@
 #   Input: nums = [1,1,1,3,3,4,3,2,4,2]
 #   Output: true
 ############################################################
+import time
 class Solution:
     def containsDuplicate(self, nums: list[int]):
-        return
+        check = set(nums)
+        return False if len(check) == len(nums) else True
 
 
 
@@ -24,6 +26,9 @@ class Solution:
 
 
 ############################Проверка############################
+st = time.time()
 print(Solution.containsDuplicate(None, [1,2,3,1]))             # -> True
 print(Solution.containsDuplicate(None, [1,2,3,4]))             # -> False
 print(Solution.containsDuplicate(None, [1,1,1,3,3,4,3,2,4,2])) # -> True
+et = time.time()
+print(f"Время выполнения кода: {et-st} секунд")
