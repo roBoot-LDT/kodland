de6cd423afbfeb320bb365ae3521c072c095c02a
roBoot <maksim.nemkov.ldt@gmail.com>
2023-05-20 17:58:47 +0300

    test

--- test.py
+++ test.py
@@ -1,12 +1,18 @@
+import discord
+from discord.ext import commands
+import os, random
 
-def twoSum(nums, target):
-    seen = {}
-    for id, number in enumerate(nums):
-        if target - number in seen:
-            return [id, seen[target-number]]
-        else:
-            seen[number] = id
-print(twoSum([2,7,11,15], 9))
-print(twoSum([3,2,4], 6))
-print(twoSum([3,3], 6))
-print(twoSum([3,2,3], 6))
+intents = discord.Intents.all()
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'We have logged in as {bot.user}')
+
+@bot.command()
+async def mem(ctx):
+    img_name = random.choice(os.listdir('/home/roboot/kodland/discord/images')) 
+    with open(f'{img_name}', 'rb') as f:
+        picture = discord.File(f)
+    await ctx.send(file=picture)
+bot.run('MTAyMDk0NTcwMDgxNDU5NDA0OA.G5Gjxz.i860sD-wsadO97J29W5J9bDlSn-ucBHY0GFVKo')
