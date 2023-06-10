341aae86219757d688fb2d0ea115b8678101a95a
roBoot <maksim.nemkov.ldt@gmail.com>
2023-06-10 17:10:17 +0300

    Co-authored-by: Papuasenvdd <Papuasenvdd@users.noreply.github.com> Co-authored-by: Miroshkevich <Miroshkevich@users.noreply.github.com> Co-authored-by: Lera874 <Lera874@users.noreply.github.com> Co-authored-by: djuneyd <djuneyd@users.noreply.github.com> Co-authored-by: Mopsyatin <Mopsyatin@users.noreply.github.com>

--- test.py
+++ test.py
@@ -1,18 +1,1 @@
-import discord
-from discord.ext import commands
-import os, random
-
-intents = discord.Intents.all()
-bot = commands.Bot(command_prefix='$', intents=intents)
-
-@bot.event
-async def on_ready():
-    print(f'We have logged in as {bot.user}')
-
-@bot.command()
-async def mem(ctx):
-    img_name = random.choice(os.listdir('/home/roboot/kodland/discord/images')) 
-    with open(f'{img_name}', 'rb') as f:
-        picture = discord.File(f)
-    await ctx.send(file=picture)
-bot.run('MTAyMDk0NTcwMDgxNDU5NDA0OA.G5Gjxz.i860sD-wsadO97J29W5J9bDlSn-ucBHY0GFVKo')
+i9 = input('test')
